359fdf3a9e51d009de250c8a826f463a5ca6638a
vsdum <danmarkhunter13072006@gmail.com>
2022-08-27 22:37:37 +0300

    Version 1.1

--- src/launcher.js
+++ src/launcher.js
@@ -1,6 +1,6 @@
 ConfigureMultiplayer({
 	name: "Piglin Bartering Viewer",
-	version: "1.0",
+	version: "1.1",
 	isClientOnly: true
 });
 
@@ -14,10 +14,11 @@ ModAPI.addAPICallback("RecipeViewer", function(api) {
 	if(scope.KEX != null) Launch(scope);
 });
 ModAPI.addAPICallback("KernelExtension", function(api) {
-	scope.KEX = api;
-	if(scope.RV != null) {
-		if(typeof api.getKEXVersionCode === "function" && api.getKEXVersionCode() >= 130) {
-			Launch(scope);
-		} else Logger.Log("Failed to launch Piglin Bartering Viewer. You must have at least 1.2 version of KernelExtension!", "PBV");
-	}
+	if(typeof api.getKEXVersionCode === "function") {
+		let code = api.getKEXVersionCode();
+		if(code >= 300) {
+			scope.KEX = api;
+			if(scope.RV != null) Launch(scope);
+		} else Logger.Log("Failed to launch Piglin Bartering Viewer. You must have at least 3.0 version of KernelExtension, you have: " + code.toString().split("").join("."));
+	} else Logger.Log("Failed to launch Piglin Bartering Viewer. You must have at least 3.0 version of KernelExtension, you have: <1.3.0");
 });
